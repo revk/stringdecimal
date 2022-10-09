1a8dd088e27cac9aa426d619a9d6459875b05c9b
RevK <github@k.gg>
2022-10-09 14:43:44 +0100

    Typo

--- stringdecimal.h
+++ stringdecimal.h
@@ -53,7 +53,7 @@ typedef enum {
 // Decimal places formatting options (used with "places" argument)
 #define	SD_FORMATS	"-*-eSI/%"
 typedef enum {
-   SD_FORMAT_DEFAULT = 0,       // Default (EXACT if places==0, else places is set to -3 and LIMIT used)
+   SD_FORMAT_DEFAULT = 0,       // Default (EXACT if places!=0, else places is set to -3 and LIMIT used)
    SD_FORMAT_EXACT = '=',       // Exact number of places, places can even be -ve to round left of decimal
    SD_FORMAT_INPUT = '*',       // Exact number of places, but use max places from input (+places specified)
    SD_FORMAT_LIMIT = '-',       // Limit number of places, not padded (-ve means no limit on non divide, and guess length for divide)
